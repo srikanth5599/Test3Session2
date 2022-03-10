7220c275f8854a596a1ff2226f9f0858822ac3db
M.srikanth srikanth <97142287+srikanth5599@users.noreply.github.com>
2022-03-10 08:34:59 +0000

    completed

--- p1final.c
+++ p1final.c
@@ -1,1 +1,39 @@
+#include <stdio.h> 
 #include <stdio.h>
+
+int gcd(int a, int b) {
+  int t;
+  while(b != 0) {
+    t = b;
+    b = a%b;
+    a = t;
+  }
+  return a;
+}
+
+void input(int *num1, int *den1, int *num2, int *den2) {
+  printf("Enter a fraction 1 : ");
+  scanf("%d/%d", num1, den1);
+  printf("Enter a fraction 2 : ");
+  scanf("%d/%d", num2, den2);
+}
+
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3) {
+  *num3 = num1*den2 + num2*den1;
+  *den3 = den1*den2;
+  int g = gcd(*num3, *den3);
+  *num3 = *num3/g;
+  *den3 = *den3/g;
+}
+
+void output(int num1, int den1, int num2, int den2, int num3, int den3) {
+  printf("The sum of two fractions %d/%d + %d/%d = %d/%d\n", num1, den1, num2, den2, num3, den3);
+}
+
+int main() {
+  int x1, x2, y1, y2, ans1, ans2;
+  input(&x1, &x2, &y1, &y2);
+  add(x1, x2, y1, y2, &ans1, &ans2);
+  output(x1, x2, y1, y2, ans1, ans2);
+  return 0;
+}
--- p7final.c
+++ p7final.c
@@ -1,21 +1,63 @@
+/*
+7. Write a program to add two fractions.
+struct _fraction
+{
+int num,den;
+};
+typedef _fraction Fraction;
+int find_gcd(int a, int b);
+Fraction input_fraction();
+Fraction add_fractions(Fraction f1, Fraction f2)
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum)
+*/
 #include<stdio.h>
 struct _fraction
 {
-  int num,den;
+int num,den;
 };
 typedef struct _fraction Fraction;
-  int gcd(int a,int b)
-  {
-    int t;
-    while (b != 0)
-      {
-        t = b;
-        b = a%b;
-        a = t;
-      }
-    return a;
+int find_gcd(int a,int b)
+{
+int gcd=0;
+for(int i=1;i<=a || i<=b;i++)
+{
+if(a%i==0 && b%i==0)
+gcd=i;
+}
+printf("GCD=%d\n",gcd);
+return gcd;
+}
+Fraction input()
+{
+Fraction a;
+printf("Enter a fraction number =");
+scanf("%d%d",&a.num,&a.den);
+return a;
+}
+Fraction add(Fraction f1, Fraction f2)
+{
+Fraction f3;
+f3.num=f1.num*f2.den+f2.num*f1.den;
+f3.den=f1.den*f1.den;
+  printf("num=%d\n",f3.num);
+  printf("den=%d\n",f3.den);
+int g=find_gcd(f3.num,f3.den);
+f3.num=f3.num/g;
+f3.den=f3.den/g;
+return f3;
+}
+void output(Fraction sum)
+{
+printf("sum of two fraction number is %d/%d\n\n",sum.num,sum.den);
 }
 int main()
 {
-  printf("%d\n",gcd(16,24));
+Fraction f1=input();
+Fraction f2=input();
+Fraction sum=add(f1,f2);
+output(sum);
+main();
+return 0;
+
+
 }
